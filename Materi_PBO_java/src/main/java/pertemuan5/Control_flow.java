6878bcf4cee71167ebdedfd1aaadba92b1dd9440
A-11 <A-11@A-11>
2024-05-13 11:10:20 +0700

    pertemuan 7

--- Materi_PBO_java/src/main/java/pertemuan5/Control_flow.java
+++ Materi_PBO_java/src/main/java/pertemuan5/Control_flow.java
@@ -14,6 +14,29 @@ import java.util.Scanner;
  */
 public class Control_flow {
     public static void main(String[] args) {
-        Scanner keybroad = new Scanner(System.in);
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Tuliskan pesan di sini, saya akan tampilkan berulang.");
+        System.out.print("Tuliskan Pesan anda paduka : ");
+        String message = keyboard.nextLine();
+
+        // Menggunakan kelas Random untuk menghasilkan bilangan acak
+        Random mesinRandom = new Random();
+        int jumlahPengulangan = mesinRandom.nextInt(20) + 1; // Menghasilkan bilangan dari 1 sampai 20
+        
+        System.out.println("-----------------------------------------------------------------------------");
+        System.out.println("\nJumlah pengulangan yang didapatkan = " + jumlahPengulangan);
+
+        if (jumlahPengulangan >= 10) { // jika sama dengan lebih dari 10
+            System.out.println("\n\tBilangan random yang didapat terlalu besar, pesan akan diulang 5 kali.");
+            jumlahPengulangan = 5; // maka akan di cetak sebanyak 5 kali
+        }
+
+        System.out.println("\nPesan diulang sebnyak : " + jumlahPengulangan + " kali\n");
+        System.out.println("-----------------------------------------------------------------------------");
+
+        for (int n = 0; n < jumlahPengulangan; n++) { // di mulai dari index ke 0
+            System.out.println(n + ". " + message);
+        }
     }
 }
