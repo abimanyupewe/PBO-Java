c59b8549f2d21649ec7b87b4709e68c8d6797086
A-10 <A-10@A-10>
2024-05-27 11:07:21 +0700

    UML dan metode

--- Materi_PBO_java/src/main/java/pertemuan7/LatihanKelas.java
+++ Materi_PBO_java/src/main/java/pertemuan7/LatihanKelas.java
@@ -27,17 +27,24 @@ public class LatihanKelas { // TokoBuku
         System.out.println("Judul " + buku1.judul);
         buku1.author.sebutNama();
         System.out.println("Tahun Terbit " + buku1.tahunPenerbitan);
-        System.out.println("Harga " + buku1.harga.jual);
+        System.out.println("Harga " + buku1.harga.getJual());
+        buku1.harga.setJual(120000); // untuk mengubah harga buku
         System.out.println("Penerbit " + buku1.penerbit.nama);
         System.out.println(buku1.penerbit.alamat);
+        System.out.println("Harga jual buku 1 = " + buku1.harga.getJual());
         System.out.println("========================== Buku 2 ===============================");
         System.out.println("Judul " + buku2.judul);
         System.out.println("Author " + buku2.author.nama);
         System.out.println("Tahun Terbit " + buku2.tahunPenerbitan);
-        System.out.println("Harga " + buku2.harga.jual);
+//        System.out.println("Harga " + buku2.harga.jual);
+        System.out.println("Harga " + buku2.harga.getBeli());
         System.out.println("Penerbit " + buku1.penerbit.nama);
         System.out.println(buku1.penerbit.alamat);
+        System.out.println("Harga jual buku 2 = " + buku2.harga.getJual());
         System.out.println("=================================================================");
+
+//        Harga hargabaru = new Harga()
+        buku1.terjual();
 
     }
 }
@@ -49,10 +56,32 @@ class Buku {
     int tahunPenerbitan = 1855;
     Harga harga = new Harga();
     Corporation_book penerbit = new Corporation_book();
+
+    public void terjual() {
+        System.out.println("Buku terjual dengan harga " + harga.getBeli());
+    }
 }
 
 class Harga {
 
-    double jual = 200000.00;
-    double beli = 10000.00;
+    private double jual = 200000.00;
+    private double beli = 10000.00;
+
+    //    menggunakan get dan seter di insert code
+    public double getJual() {
+        return jual;
+    }
+
+    public void setJual(double jual) {
+        this.jual = jual;
+    }
+
+    public double getBeli() {
+        return beli;
+    }
+
+    public void setBeli(double beli) {
+        this.beli = beli;
+    }
+
 }
